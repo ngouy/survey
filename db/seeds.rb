1bfce0d431776aed175eb9f31131c1ec9c09998f
ngouy <nathangouy@free.fr>
2017-09-18 10:02:36 +0200

    adding some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+survey = Survey.create!(name: "Test Survey")
+
+survey.add_questions([
+  "question_0",
+  "question_1",
+  "question_2",
+  "question_3",
+]);
+
+questions = survey.ordered_questions
+
+3.times do |question_index|
+  4.times { |index| questions[question_index].add_answer("question_#{question_index}_answer_#{index}", next_question_id: questions[question_index + 1].id) }
+end
+
+4.times { |index| questions[3].add_answer "question_3_answer_#{index}" }
+
+# make the last first question's answer a leaf
+questions.first.answers.last.update_attributes(next_question_id: nil)
+
+
+10.times do
+  feedback = Feedback.create!(survey: survey)
+  while feedback.next_question do
+    feedback.new_answer(feedback.next_question.answers.to_a.sample)
+  end
+end
+
+
